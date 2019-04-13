d033a2c40e9d0dd17742a8782e3d643bc10cc4a6
SnowflakeI <indma@gmail.com>
2019-04-12 23:59:29 -0400

    version with all major functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,42 +27,29 @@ function createButtons(){
         btn.setAttribute("value",str[i]);
         btn.setAttribute("text",str[i]);
         btn.innerHTML =str[i];
-        btn.setAttribute('onclick', "validateChar('" + btn.innerHTML + "')");
+        // btn.setAttribute('onclick', "validateChar('" + btn.innerHTML + "')");
+        btn.setAttribute('onclick',"validateChar( '" + btnId + "')");
         btn.className="btnKey";
         grid.appendChild(btn);
         // document.body.appendChild(btn);
     }
 
     document.body.appendChild(grid);
-}
 
-/*******************************function to validate the user entered character*********************************** */
-function validateChar(elementId){
-     debugger;
-    var useChoise = elementId;
-
-    console.log(useChoise);
-
-    for (var i = 0 ; i < compChoise.length; i++) {
-        if (compChoise[i] === useChoise){
-            compWord[i] = useChoise;
-        }
-    }
-
-    maxTurns = maxTurns - 1;
-}
+} // end of function createButtons
 
 
 
-/****************************************function to random generate the word************************************************************** */
+/****************************************function to random generate the word***************Cl*********************************************** */
 function generateWord(){
 
     
     var wordsArray=["quidditch","muggle","Harry Potter","Polyjuice","Azkaban","Patronus","Hogwarts","Gryffindor","Slytherin","Ravenclaw","Hufflepuff",
                      "Basilisk","Death Eater","Diagon Alley","Dumbledore","Firebolt","Goblet of Fire","Golden Snitch","Hermione","Ron Weasley"];
 
-    // compChoise = wordsArray[Math.floor(Math.random()*wordsArray.length)]; 
-    compChoise ="Goblet of Fire"
+    compChoise = "";
+                     // compChoise = wordsArray[Math.floor(Math.random()*wordsArray.length)]; 
+    compChoise ="FIRE"
     compWord = [];
     console.log(compChoise);     
     
@@ -82,9 +69,7 @@ function generateWord(){
     }
 
     var printWord ="";
-    for (i=0; i < compWord.length; i++){
-       printWord = printWord + compWord[i] + ' ';
-    }
+    printWord = compWord.join(' ');
 
     //  debugger;
     
@@ -94,5 +79,139 @@ function generateWord(){
     
     pWordGuess.textContent = printWord;
     
+} //end of function generateWord
+
+/*******************************function to validate the user entered character*********************************** */
+function validateChar(elementId){
+    // debugger;
+
+   var buttonId = document.getElementById(elementId) ;
+   var useChoise = buttonId.innerHTML;
+
+   console.log(useChoise);
+   var letterFound = false;
+
+   console.log(useChoise);
+
+   for (var i = 0 ; i < compChoise.length; i++) {
+       if (compChoise[i] === useChoise){
+           letterFound = true;
+           compWord[i] = useChoise;
+       }
+   }
+// debugger;
+  if(letterFound === false){
+   maxTurns = maxTurns - 1;
+   var noOfTurns = document.getElementById("textTri");
+   noOfTurns.innerHTML = maxTurns;
+  } 
+  else {
+      var modifiedWord = compWord.join(' ');
+      var pWordGuess = document.getElementById("wordGuess"); //element id for pragraph wordGuess
+      console.log(modifiedWord);
+      
+      pWordGuess.textContent = modifiedWord;
+  }
+  buttonId.disabled = true;
+
+  var turnValidate = validateTurns();
+  
+} // end of function validateChar
+
+/***************************************funtion to validate the no of available turns****************************** */
+function validateTurns(){
+    debugger;
+    if (maxTurns === 0 ){
+        var audio = new Audio();
+        audio.src = 'http://soundfxcenter.com/movies/harry-potter/8d82b5_Harry_Potter_Expelliarmus_Sound_Effect.mp3';
+        console.log(audio.src);
+        audio.play();
+        var confirmPlay = confirm("You lost! Do you want to play again");
+
+        if (confirmPlay == true) {
+            initiateGame();
+            console.log("Try again");
+        } 
+        else {
+            endGame();
+        }
+    }
+    else {
+        guessCompletWord();
+    }
+} //end of fucntion validateTurns
+
+/******************************************function to reinitiate game********************* */
+
+function initiateGame(){
+    var btnClass = document.getElementsByClassName("btnKey");
+
+    for (var i = 0; i < btnClass.length; i++) {
+        btnClass[i].disabled = false;
+    }
+    maxTurns = 10;
+    generateWord();
+    
+}// end of function initiateGame
+
+/********************************************function to check if user guess all the letters********************* */
+
+function guessCompletWord(){
+    var compChoiseTemp = compChoise.replace(/ /g,'\xA0');
+    var compChoiseArray = compChoiseTemp.split("");
+    var isCompleted = true;
+
+    console.log(compChoiseArray);
+    debugger;
+    if(compChoiseArray.length == compWord.length){
+        for(var i = 0; i < compChoiseArray.length; i++ ){
+            if (compChoiseArray[i] !== compWord[i]) {
+                console.log("test" + compWord[i]);
+                isCompleted = false ;
+                break;
+            }
+        }
+               
+    }
+    if (isCompleted === true) {
+      displayAlert("W");
+    } 
+}
+    
+function displayAlert(optionId) {
+    if (optionId = 'W'){
+        var divAlert = document.getElementById("modal");
+        divAlert.style.display = "block";
+
+        var alertText = document.getElementById("alertId");
+        alertText.textContent = "You Won!!!!!!!, Do you want play again?";
+
+        var alterImage= document.getElementById("alertImg");
+        alterImage.src = "https://s4.scoopwhoop.com/anj/sw/7543a1cb-193f-4745-9daf-4e607e806259.jpg";
+    
+        //  alert("Word Complete");
+    }
 }
 
+/**********************************************function onclik of yes button in the alert************************************ */
+function yesClick(){
+    var divAlert = document.getElementById("modal");
+    divAlert.style.display = "none";
+    initiateGame();
+
+}
+
+function noClick(){
+    var divAlert = document.getElementById("modal");
+    divAlert.style.display = "none";
+    endGame();
+}
+
+function endGame(){
+    alert ("Bye Bye");
+    var btnClass = document.getElementsByClassName("btnKey");
+
+    for (var i = 0; i < btnClass.length; i++) {
+        btnClass[i].disabled = true;
+    }
+}
